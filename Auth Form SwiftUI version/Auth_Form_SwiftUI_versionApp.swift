2486f21fd0bbdd35192d44b58fc5b9271579f461
Dmitry Novosyolov <i1710@icloud.com>
2021-03-10 21:26:50 +0200

    Initial Commit

---
+++ Auth Form SwiftUI version/Auth_Form_SwiftUI_versionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auth_Form_SwiftUI_versionApp.swift
+//  Auth Form SwiftUI version
+//
+//  Created by Dmitry Novosyolov on 10/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Auth_Form_SwiftUI_versionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
